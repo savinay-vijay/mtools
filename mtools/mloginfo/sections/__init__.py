30329b02cef2a8e8222961b55da09bd6d6794660
miteshgosavi <mitesh.gosavi@mongodb.com>
2019-06-28 11:54:27 +0100

    .gitignore fix

---
+++ mtools/mloginfo/sections/__init__.py
@@ -0,0 +1,7 @@
+from .connection_section import ConnectionSection
+from .distinct_section import DistinctSection
+from .query_section import QuerySection
+from .restart_section import RestartSection
+from .rs_info_section import RsInfoSection
+from .rs_state_section import RsStateSection
+from .transaction_section import TransactionSection
---
+++ mtools/version.py
@@ -0,0 +1,4 @@
+#!/bin/python
+"""Mtools version."""
+
+__version__ = '1.5.4-dev'
